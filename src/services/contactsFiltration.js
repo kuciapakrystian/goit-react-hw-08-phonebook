c548b8dc6ec81a35997171b4664dd3c13487c873
kuciapakrystian <kuciapakrystian@outlook.com>
2024-05-09 17:22:08 +0200

    update

---
+++ src/services/contactsFiltration.js
@@ -0,0 +1,13 @@
+const contactsFiltration = (contacts = [], filter = '') => {
+    const normalizedFilter = filter.toLowerCase();
+  
+    return contacts
+      .filter(
+        ({ name, number }) =>
+          name.toLowerCase().includes(normalizedFilter) ||
+          number.includes(normalizedFilter)
+      )
+      .sort((a, b) => Number(a.favorite) - Number(b.favorite));
+  };
+  
+  export default contactsFiltration;
