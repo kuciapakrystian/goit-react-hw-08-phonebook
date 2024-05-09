1c2639f5068a9408bf171255f99d045a9895aba9
kuciapakrystian <kuciapakrystian@outlook.com>
2024-05-09 14:17:43 +0200

    update

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import Header from 'components/Header/Header';
+import { Container } from './MainLayout.styled';
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <Suspense fallback={<h2>Loading...</h2>}>
+            <Outlet />
+          </Suspense>
+        </Container>
+      </main>
+    </>
+  );
+};
---
+++ src/MainLayout/MainLayout.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const Container = styled.main`
+  flex: 1;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 5px;
+  padding-right: 5px;
+  padding-top: 10vh;
+  max-width: 1440px;
+`;
+
+export { Container };
