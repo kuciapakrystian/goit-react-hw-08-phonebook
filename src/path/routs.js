6e890a101f30430e92d145f0e4e11232a2c3b72f
kuciapakrystian <kuciapakrystian@outlook.com>
2024-05-09 17:28:00 +0200

    update

--- src/path/routs.js
+++ src/path/routs.js
@@ -1,12 +1,12 @@
 export const routsPath = {
-    home: '/',
-    logIn: 'logIn',
-    signUp: 'signUp',
-    contacts: 'contacts',
-    favorites: 'favorites',
-  };
-  
-  export const redirectPath = {
-    toLogIn: '/logIn',
-    toContacts: '/contacts',
-  };
+  home: '/',
+  logIn: 'logIn',
+  signUp: 'signUp',
+  contacts: 'contacts',
+  favorites: 'favorites',
+};
+
+export const redirectPath = {
+  toLogIn: '/logIn',
+  toContacts: '/contacts',
+};
